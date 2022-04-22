64554820bf08cb4d84ad387db25d43d6f4c5931e
Boris Bashkintsev <bazis13@yandex.ru>
2022-04-22 22:43:52 +0300

    подпись к булкам. Верх и низ

--- src/components/burger-constructor/drag-element/drag-element.js
+++ src/components/burger-constructor/drag-element/drag-element.js
@@ -8,6 +8,16 @@ class DragElement extends Component {
 		super(props);
 	}
 
+	renderName = (name, type) => {
+		if(type === 'top') {
+			name = name + '\n(верх)';
+		}
+		if(type === 'bottom') {
+			name = name + '\n(низ)';
+		}
+		return name;
+	}
+
   	render() {
     	return (
       		<div className = {styles.container}>
@@ -18,7 +28,7 @@ class DragElement extends Component {
 				</div>          		
 				<ConstructorElement
 					type = {this.props.bunType}
-					text = {this.props.ingredient.name}
+					text = {this.renderName(this.props.ingredient.name, this.props.bunType)}
 					thumbnail = {this.props.ingredient.image_mobile}
 					price = {this.props.ingredient.price}
 				/>
